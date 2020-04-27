d1333376519261b8a0c7a2e2dcce9f8e3fdd532c
Maxim Kolesnik <makleso6@gmail.com>
2020-04-27 14:11:43 +0300

    Update MiioTests.swift

--- Tests/MiioTests/MiioTests.swift
+++ Tests/MiioTests/MiioTests.swift
@@ -6,22 +6,7 @@ final class MiioTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        let value = 0xff
-        print(value)
-        print(0xff)
-        let expectation = XCTestExpectation(description: "handshake")
-
-        do {
-            let service = try UDPNetworkService()
-            service.handshake({
-                print("handshaked")
-                expectation.fulfill()
-            })
-        } catch {
-            print(error)
-        }
         
-        wait(for: [expectation], timeout: 10.0)
     }
 
     static var allTests = [
